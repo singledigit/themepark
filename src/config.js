32c6985eeee54d21cc89f06fd2eeb3b299081f5d
Eric Johnson <ericdj@amazon.com>
2023-08-18 16:23:09 -0600

    Module 2

--- src/config.js
+++ src/config.js
@@ -13,9 +13,9 @@ export const appConfig = {
   initStateAPI: 'https://cxvjcyuikc.execute-api.us-west-2.amazonaws.com/Prod/InitState/', // e.g. https://12abcdef89.execute-api.us-west-2.amazonaws.com/Prod/InitState/
   // MODULE 2 - REALTIME
   iot: {
-    poolId: '', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
-    host: '', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
-    region: '' // e.g. 'us-west-1'
+    poolId: 'us-west-2_xm7mbk4gR', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
+    host: 'a3q0om53d38c27-ats.iot.us-west-2.amazonaws.com', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
+    region: 'us-west-2' // e.g. 'us-west-1'
   },
   // MODULE 3 - PHOTOS
   photoUploadURL: '', // e.g. https://12abcdfg89.execute-api.us-west-2.amazonaws.com/Prod/Upload
